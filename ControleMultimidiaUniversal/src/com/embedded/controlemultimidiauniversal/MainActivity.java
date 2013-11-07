d03b1b5bd48a0238e82a1004734d66e94cd00725
FelipeLindemberg <felipelindemberg@sony02.(none)>
2013-11-07 17:07:23 -0300

    Add logica para selecionar tv e som

--- ControleMultimidiaUniversal/src/com/embedded/controlemultimidiauniversal/MainActivity.java
+++ ControleMultimidiaUniversal/src/com/embedded/controlemultimidiauniversal/MainActivity.java
@@ -3,21 +3,59 @@ package com.embedded.controlemultimidiauniversal;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.CheckBox;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	private CheckBox checkTv, checkSom;
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+		setUpSelectSom();
+		setUpSelectTv();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	private void setUpSelectTv() {
+		checkTv = (CheckBox) findViewById(R.id.checkBoxTv);
+		checkTv.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				checkTv.setChecked(true);
+
+				checkSom = (CheckBox) findViewById(R.id.checkBoxSom);
+				checkSom.setChecked(false);
+
+			}
+		});
+
+	}
+
+	private void setUpSelectSom() {
+		checkSom = (CheckBox) findViewById(R.id.checkBoxSom);
+		checkSom.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				checkSom.setChecked(true);
+				checkTv = (CheckBox) findViewById(R.id.checkBoxTv);
+				checkTv.setChecked(false);
+
+			}
+		});
+
+	}
+
 }
