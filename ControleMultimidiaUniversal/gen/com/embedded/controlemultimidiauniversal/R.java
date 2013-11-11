b78842334cbeb2750403e6ffc429a9f39bd3ad94
FelipeLindemberg <felipelindemberg@sony02.(none)>
2013-11-11 13:58:20 -0300

    Passando para relative layout

--- ControleMultimidiaUniversal/gen/com/embedded/controlemultimidiauniversal/R.java
+++ ControleMultimidiaUniversal/gen/com/embedded/controlemultimidiauniversal/R.java
@@ -57,17 +57,14 @@ public final class R {
         public static final int volume_up_pressed=0x7f02001b;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000b;
-        public static final int buttonDownChannel=0x7f09000a;
-        public static final int buttonDownVolume=0x7f090006;
-        public static final int buttonMute=0x7f090007;
+        public static final int action_settings=0x7f090008;
+        public static final int buttonDownChannel=0x7f090006;
+        public static final int buttonDownVolume=0x7f090003;
+        public static final int buttonMute=0x7f090005;
         public static final int buttonUpChannel=0x7f090004;
-        public static final int buttonUpVolume=0x7f090008;
+        public static final int buttonUpVolume=0x7f090007;
         public static final int checkBoxSom=0x7f090001;
         public static final int checkBoxTv=0x7f090000;
-        public static final int tableRow1=0x7f090003;
-        public static final int tableRow2=0x7f090005;
-        public static final int tableRow3=0x7f090009;
         public static final int textViewNameRoom=0x7f090002;
     }
     public static final class layout {
