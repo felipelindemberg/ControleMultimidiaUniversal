8b3b4b2b54c10a6ed8921d28b1183c4fc6aba051
felipe.mdrs <felipe.miranda.mdrs@gmail.com>
2013-11-11 16:56:30 -0300

    Criação de Enum para Equipment e atualização da lista de comandos^C

---
+++ ControleMultimidiaUniversal/src/com/embedded/controlemultimidiauniversal/Equipment.java
@@ -0,0 +1,11 @@
+package com.embedded.controlemultimidiauniversal;
+
+/**
+ * Lista de equipamentos a serem controlados.
+ * 
+ * @author felipemm
+ *
+ */
+public enum Equipment {
+	TV, SOM;
+}
--- ControleMultimidiaUniversal/src/com/embedded/controlemultimidiauniversal/net/Command.java
+++ ControleMultimidiaUniversal/src/com/embedded/controlemultimidiauniversal/net/Command.java
@@ -7,24 +7,5 @@ package com.embedded.controlemultimidiauniversal.net;
  * 
  */
 public enum Command {
-	POWER("POWER"), UPVOLUME("UPVOLUME"), DOWNVOLUME("DOWNVOLUME"), UPCHANNEL(
-			"UPCHANNEL"), DOWNCHANNEL("DOWNCHANNEL"), MUTE("MUTE");
-
-	private String name;
-
-	private Command(String stringVal) {
-		name = stringVal;
-	}
-
-	public String toString() {
-		return name;
-	}
-
-	public static String getEnumByString(String code) {
-		for (Command e : Command.values()) {
-			if (code == e.name)
-				return e.name();
-		}
-		return null;
-	}
+	POWER, UPVOLUME, DOWNVOLUME, DOWNCHANNEL, MUTE;
 }
